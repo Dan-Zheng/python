e796a1abaefc8c90d34738e36ed213011f70e0bf
Dan Zheng <zheng321@purdue.edu>
2015-10-25 03:17:10 -0400

    Add to_uppercase, modify pick_word and to_lowercase

--- to_uppercase/to_uppercase.py
+++ to_uppercase/to_uppercase.py
@@ -3,12 +3,12 @@
 import pyperclip
 
 def main():
-    string = raw_input('Enter a string to convert to lowercase: ')
-    print '\nLowercase: ' + string.lower() + '\n'
+    string = raw_input('Enter a string to convert to uppercase: ')
+    print '\nUppercase: ' + string.upper() + '\n'
 
     copy_confirm = raw_input('Copy to clipboard? (y/n)\n').lower()
     if copy_confirm == 'yes' or copy_confirm == 'y':
-        pyperclip.copy(string.lower()) # copies the string to clipboard
+        pyperclip.copy(string.upper()) # copies the string to clipboard
         print 'Successfully copied to clipboard.'
     else:
         print 'Not copied to clipboard.'
